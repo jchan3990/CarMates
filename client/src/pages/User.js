3e95fc30bc39b6cac59c212e71528c3111bfe5d0
jchan3990 <jonchan3990@gmail.com>
2020-12-22 21:59:55 -0800

    Finished User Cards

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,16 +1,64 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import { Card, Icon, Image, List } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth.js';
 import { FETCH_USER_QUERY } from '../utils/graphql.js';
 
 const User = (props) => {
+  const username = props.match.params.username;
 
-  return (
-    <div>
-      <h1>Test</h1>
-    </div>
-  )
+  const { data: {getUser: userData} = {} } = useQuery(FETCH_USER_QUERY, {
+    variables: { username }
+  });
+
+  let userCard;
+  if (!userData) {
+    userCard = <p>Fetching user profile</p>;
+  } else {
+    const { id, createdAt, username, email, avatar, carYear, carMake, carModel, zipCode } = userData;
+    let joined = new Date(createdAt);
+    joined = joined.getFullYear();
+    let emailLink = `mailto:${email}`;
+
+    userCard = (
+      <Card>
+        <Image src={avatar} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{username}</Card.Header>
+          <Card.Meta>
+            <span className='date'>Joined in {joined}</span>
+          </Card.Meta>
+          <Card.Description>
+          <List>
+            <List.Item>
+              <List.Icon name='car' />
+              <List.Content>{carYear} {carMake} {carModel}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name='marker' />
+              <List.Content>{zipCode}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name='mail' />
+              <List.Content>
+                <a href={emailLink}>{email}</a>
+              </List.Content>
+            </List.Item>
+          </List>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name='user' />
+            22 Friends
+          </a>
+        </Card.Content>
+      </Card>
+    )
+  }
+
+  return userCard;
 }
 
 export default User;
