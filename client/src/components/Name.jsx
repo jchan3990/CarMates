9769b4d363412491ed931bc57134304eb3dba744
jchan3990 <jonchan3990@gmail.com>
2021-01-17 22:51:17 -0800

    Clicking name goes to user profile

--- client/src/components/Name.jsx
+++ client/src/components/Name.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
 
 const Name = ({ name }) => {
+
   return (
-    <div>
-      <span id='name'>{name}</span>
-    </div>
+    // <a id='name' href={`/users/${name}`}>{name}</a>
+    <Link id='name' to={`/users/${name}`}>{name}</Link>
   )
 }
 
