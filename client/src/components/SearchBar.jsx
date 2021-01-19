b9ae798d276bb5a0de37e4c371bc12b3ab361760
jchan3990 <jonchan3990@gmail.com>
2021-01-19 00:20:54 -0800

    Fixed warning from dynamic search

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -6,13 +6,15 @@ const SearchBar = ({ handleSearchTerm, currPath, updateLocation }) => {
   let location = useLocation();
   let locationPath = location.pathname
 
+  useEffect(() => {
+    if (currPath !== locationPath) {
+      Array.from(document.querySelectorAll('#search-bar')).forEach(input => input.value = '');
+      updateParentLocation();
+    }
+  }, [locationPath])
+
   const updateParentLocation = () => {
     updateLocation(locationPath);
-  }
-
-  if (currPath !== locationPath) {
-    Array.from(document.querySelectorAll('#search-bar')).forEach(input => input.value = '');
-    updateParentLocation();
   }
 
   return (
