5b363811e1b4402d8511889e2f5d725a9e55ac4b
jchan3990 <jonchan3990@gmail.com>
2021-03-04 00:03:37 -0800

    Set default map center to San Jose

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 import { gMapsKey } from '../../../config.js';
 
 const Map = () => {
+  const defaultCenter = {
+    lat: 37.334160,
+    lng: -121.888682
+  }
+
+  const [center, setCenter] = useState(geocode === undefined ? defaultCenter : geocode);
   const style = {
     height: "100%",
     width: "100%",
-  }
-
-  const defaultCenter = {
-    lat: 41.3851,
-    lng: 2.1734
   }
 
   return (
